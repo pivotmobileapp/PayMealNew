cb06dadd8af486e7c3a19c520816b972ef87b0fe
pivotmobileapp <jupiter@pivotmobileapp.com>
2020-06-01 06:40:35 -0400

    zip code removed

--- platforms/browser/www/js/template.js
+++ platforms/browser/www/js/template.js
@@ -17,9 +17,9 @@ tabbarMenu = function(){
 	    html+='<ons-tab id="tab-home" page="home.html" label="'+ t("Home") +'" icon="home" active  active-icon="home" >';
 	    html+='</ons-tab>';
 	    html+='<ons-tab id="tab-search" page="search.html" label="'+ t("Search") +'" icon="search" active-icon="search" >';
-	    html+='</ons-tab>';	    
-	    html+='<ons-tab id="tab-profile" page="profile.html" label="'+ t("Profile") +'" icon="user-alt" active-icon="user-alt" >';
-	    html+='</ons-tab>';	   
+	    html+='</ons-tab>';	  
+		html+='<ons-tab id="tab-profile" page="profile.html" label="'+ t("Profile") +'" icon="user-alt" active-icon="user-alt" >';
+	    html+='</ons-tab>';		
 	    html+='<ons-tab id="tab-cart" page="cart_temp.html" label="'+ t("Cart") +'" icon="shopping-cart" active-icon="shopping-cart" badge="" >';
 		html+='</ons-tab>';
 		html+='<ons-tab id="tab-history" page="order_list.html" label="'+ t("Order History") +'" icon="history" active-icon="history">';
